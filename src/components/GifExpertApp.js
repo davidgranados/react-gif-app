bfad6aac78660b5b4df5203e973aeb8f6530df0d
David Granados <david.granados@aguayo.co>
2021-05-28 18:37:20 -0500

    S6 - C70: Creando una lista de categorías

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+  const [newCategory, setNewCategory] = useState('');
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+  const handleNewCategoryChange = ({target}) => {
+    setNewCategory(target.value);
+  };
+  const handleAddCategory = () => {
+    setCategories((prevState) => [...prevState, newCategory]);
+    setNewCategory('');
+  };
   return (
     <>
       <h2>GifExpertApp</h2>
-      <hr/>
+      <hr />
+      <input type="text" onChange={handleNewCategoryChange} value={newCategory}/>
+      <button onClick={handleAddCategory}>Add New Category</button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
