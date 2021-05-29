9b7b8166cb8310b81b4a54dcd2f2be7a8d69aa84
David Granados <david.granados@aguayo.co>
2021-05-28 19:46:05 -0500

    S6 - C74: useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 const GifGrid = ({ category }) => {
-  const getGifs = async () => {
-    const params = {
-      q: category,
-      limit: 10,
-      api_key: "C5NwjWaDX2G3sZZVj5grW5eQm7qSon4i",
-    };
-    const endpoint = "https://api.giphy.com/v1/gifs/search";
-    var queryString = Object.keys(params)
-      .map((key) => {
-        return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-      })
-      .join("&");
-    const url = `${endpoint}?${queryString}`;
-    const response = await fetch(url);
-    const { data } = await response.json();
-    const gifs = data.map((gif) => {
-      return {
-        id: gif.id,
-        title: gif.title,
-        url: gif.images.downsized_medium.url,
+  useEffect(() => {
+    const getGifs = async () => {
+      const params = {
+        q: category,
+        limit: 10,
+        api_key: "C5NwjWaDX2G3sZZVj5grW5eQm7qSon4i",
       };
-    });
+      const endpoint = "https://api.giphy.com/v1/gifs/search";
+      var queryString = Object.keys(params)
+        .map((key) => {
+          return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+        })
+        .join("&");
+      const url = `${endpoint}?${queryString}`;
+      const response = await fetch(url);
+      const { data } = await response.json();
+      const gifs = data.map((gif) => {
+        return {
+          id: gif.id,
+          title: gif.title,
+          url: gif.images.downsized_medium.url,
+        };
+      });
 
-    console.log(gifs);
-  };
-
-  getGifs();
+      console.log(gifs);
+    };
+    getGifs();
+  }, [category]);
   return (
     <div>
       <h3>{category}</h3>
