d04c8c99d634b22fcd3ba430a9946280c9c4bcc1
David Granados <davidgranados73@gmail.com>
2021-06-01 18:56:43 -0500

    S6 - C94: Pruebas del componente AddCategory, simular submit del formulario

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -3,18 +3,33 @@ import { shallow } from "enzyme";
 import AddCategory from "../../components/AddCategory";
 
 describe("Tests on AddCategory component", function () {
-  const wrapper = shallow(<AddCategory onAddCategory={() => {}} />);
-  const event = {
-    target: {
-      value: "Hola mundo",
-    },
-  };
+  const onAddCategory = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory onAddCategory={onAddCategory} />);
+  });
+
   it("component should show correctly", function () {
     expect(wrapper).toMatchSnapshot();
   });
+
   it("should change newCategory state", function () {
+    const changeEvent = {
+      target: {
+        value: "Hola mundo",
+      },
+    };
     expect(wrapper.find("input").props().value).toEqual("");
-    wrapper.find("input").simulate("change", event);
-    expect(wrapper.find("input").props().value).toEqual(event.target.value);
+    wrapper.find("input").simulate("change", changeEvent);
+    expect(wrapper.find("input").props().value).toEqual(changeEvent.target.value);
+  });
+
+  it('should not call onAddCategory on submit if newCategory if empty', function () {
+    const submitEvent = {
+      preventDefault(){}
+    };
+    wrapper.find("form").simulate("submit", submitEvent);
+    expect(onAddCategory).not.toHaveBeenCalled();
   });
 });
