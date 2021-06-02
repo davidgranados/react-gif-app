b9c575fa40e0626b265c8ceb558b2495ceb4e19a
David Granados <davidgranados73@gmail.com>
2021-06-01 19:11:40 -0500

    S6 - C95: Pruebas del componente AddCategory, simular submit llamando una función y limpiando el input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,7 +4,17 @@ import AddCategory from "../../components/AddCategory";
 
 describe("Tests on AddCategory component", function () {
   const onAddCategory = jest.fn();
+  const changeEvent = {
+    target: {
+      value: "Hola mundo",
+    },
+  };
+  const submitEvent = {
+    preventDefault() {},
+  };
+
   let wrapper;
+
   beforeEach(() => {
     jest.clearAllMocks();
     wrapper = shallow(<AddCategory onAddCategory={onAddCategory} />);
@@ -15,21 +25,26 @@ describe("Tests on AddCategory component", function () {
   });
 
   it("should change newCategory state", function () {
-    const changeEvent = {
-      target: {
-        value: "Hola mundo",
-      },
-    };
     expect(wrapper.find("input").props().value).toEqual("");
     wrapper.find("input").simulate("change", changeEvent);
-    expect(wrapper.find("input").props().value).toEqual(changeEvent.target.value);
+    expect(wrapper.find("input").props().value).toEqual(
+      changeEvent.target.value
+    );
   });
 
-  it('should not call onAddCategory on submit if newCategory if empty', function () {
-    const submitEvent = {
-      preventDefault(){}
-    };
+  it("should not call onAddCategory on submit if newCategory if empty", function () {
     wrapper.find("form").simulate("submit", submitEvent);
     expect(onAddCategory).not.toHaveBeenCalled();
+  });
+
+  it("should call onAddCategory on and clean newCategory on submit if newCategory if not empty", function () {
+    wrapper.find("input").simulate("change", changeEvent);
+    expect(wrapper.find("input").props().value).toEqual(
+      changeEvent.target.value
+    );
+    wrapper.find("form").simulate("submit", submitEvent);
+    expect(onAddCategory).toHaveBeenCalled();
+    expect(onAddCategory).toHaveBeenCalledWith(changeEvent.target.value);
+    expect(wrapper.find("input").props().value).toEqual("");
   });
 });
